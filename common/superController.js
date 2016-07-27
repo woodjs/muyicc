1b7257cc0ef7faa14ccbe7b11fe0519ac043a47f
bugong <1761869346@qq.com>
2016-07-27 22:47:20 +0800

    修改wood

--- common/superController.js
+++ common/superController.js
@@ -1,9 +1,21 @@
+"use strict";
+
 var gu = require('guthrie-js');
 var flowController = require('./flowController');
 var masterController = new gu.controller.inherit(flowController);
 
 masterController.on('actionExecuting', function (req, res, next) {
+  //(async function () {
+  //
+  //  console.log('1111');
+  //})();
 
+  async function () {
+    "use strict";
+
+    console.log(1111);
+
+  }
   next();
 });
 
